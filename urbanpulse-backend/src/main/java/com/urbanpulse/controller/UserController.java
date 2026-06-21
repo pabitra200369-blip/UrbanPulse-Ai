1aade529c7fb2ab5d0fa93f28223a5811b3f682c
Soumya1225 <ssoumyaranjan883@gmail.com>
2026-06-21 16:28:06 +0530

    Integrated user login and registration API

--- urbanpulse-backend/src/main/java/com/urbanpulse/controller/UserController.java
+++ urbanpulse-backend/src/main/java/com/urbanpulse/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.urbanpulse.controller;
 
-import com.urbanpulse.dto.LoginRequest;
+import com.urbanpulse.dao.LoginRequest;
 import com.urbanpulse.entity.User;
 import com.urbanpulse.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,24 +17,17 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public User register(
-            @RequestBody User user) {
-
+    public User register(@RequestBody User user) {
         return userService.saveUser(user);
     }
 
     @PostMapping("/login")
-    public User login(
-            @RequestBody LoginRequest request) {
-
-        return userService.login(
-                request.getEmail(),
-                request.getPassword());
+    public User login(@RequestBody LoginRequest request) {
+        return userService.login(request.getEmail(), request.getPassword());
     }
 
     @GetMapping
     public List<User> getAllUsers() {
-
         return userService.getAllUsers();
     }
 }
--- urbanpulse-backend/src/main/java/com/urbanpulse/dao/LoginRequest.java
+++ urbanpulse-backend/src/main/java/com/urbanpulse/dao/LoginRequest.java
@@ -1,9 +1,12 @@
-package com.urbanpulse.dto;
+package com.urbanpulse.dao;
 
 public class LoginRequest {
 
     private String email;
     private String password;
+
+    public LoginRequest() {
+    }
 
     public String getEmail() {
         return email;
