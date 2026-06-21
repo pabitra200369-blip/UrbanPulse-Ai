f788c227bc03077a68fd6f74b743dd759daa2a8b
Soumya1225 <ssoumyaranjan883@gmail.com>
2026-06-21 16:07:43 +0530

    Refined auth service and role constants

--- urbanpulse-frontend/src/services/authService.js
+++ urbanpulse-frontend/src/services/authService.js
@@ -1,16 +1,19 @@
 import axiosInstance from "../api/axiosConfig";
 
 const authService = {
-
+  // Handles backend login request for the user
   login: async (email, password) => {
-    const response = await axiosInstance.post("/users/login", {
-      email,
-      password
-    });
+    try {
+      const response = await axiosInstance.post("/users/login", {
+        email,
+        password,
+      });
 
-    return response.data;
-  }
-
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
 };
 
 export default authService;
